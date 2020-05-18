dd0a818fdbeac902d5fc034cdae79e1f46e6a32a
hubertb98 <hubertb.98.hb@gmail.com>
2020-05-18 22:55:50 +0200

    7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,8 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,9 +12,10 @@ public class StreamMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
 
+
         Map<Integer, ForumUser> theResultMapOfUser = forum.getUserList().stream()
                 .filter(user -> user.getSex() == 'M')
-                .filter(user -> 2020 - user.getBirthDate().getYear() > 20)
+                .filter(user -> Period.between(user.getBirthDate(), LocalDate.now()).getYears() > 20)
                 .filter(user -> user.getPublishedPostQuantity() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
 
