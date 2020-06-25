6d1d8df8c5dff8c751c3e4691dab267237a10266
hubertb98 <hubertb.98.hb@gmail.com>
2020-06-26 00:54:01 +0200

    13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreStream.java
@@ -0,0 +1,16 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreStream {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String movieTitlesSeparatedByExclamationMark = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining(" ! ", "", ""));
+
+        System.out.println(movieTitlesSeparatedByExclamationMark);
+
+    }
+}
